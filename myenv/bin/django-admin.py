11a67662bb231d3f4cfc4c51d4eda084559e13f8
Jeremy Hilado <jhilado89@gmail.com>
2020-05-08 03:13:25 -0700

    init commit

---
+++ myenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/Jeremy/Documents/seir224/unit04/week12/capstone-project/ga-capstone-project-backend/myenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
