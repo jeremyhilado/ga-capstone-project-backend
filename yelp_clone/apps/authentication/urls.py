4b51ade01362f1b20646c6740d7d0126799d54f4
Jeremy Hilado <jhilado89@gmail.com>
2020-05-12 13:10:22 -0700

    working backend

--- yelp_clone/apps/authentication/urls.py
+++ yelp_clone/apps/authentication/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from apps.authentication.views import RegistrationAPIView, LoginAPIView
 
 urlpatterns = [
-    url(r'^user/register/$', RegistrationAPIView.as_view(), name='register'),
-    url(r'^user/login/$', LoginAPIView.as_view(), name='login'),
+    url(r'^users/register/$', RegistrationAPIView.as_view(), name='register'),
+    url(r'^users/login/$', LoginAPIView.as_view(), name='login'),
 ]
 
